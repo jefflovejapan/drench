1f6c443aa3bd60c4d1a9b055f79f6d3495daa3c7
Jeff Blagdon <jeffblagdon@gmail.com>
2013-10-18 19:29:06 -0400

    Got HTTP request working

--- torrent.py
+++ torrent.py
@@ -10,35 +10,37 @@ import urllib
 
 class torrent():
 
-    def __init__(self, tdict):
+    def __init__(self, tdict, port=55308):
         self.tdict = tdict
+        self.port = port
+        self.r = None
 
     def get_request(self):
+        # payload = {}
         assert self.tdict['info']
         hashed_info = hashlib.sha1(tparser.bencode(self.tdict['info']))
-        # Get a hashy string
-        url_string = urllib.urlencode({'myhashything': hashed_info.digest()})
-        print url_string.strip('myhashything=')
+        hash_string = hashed_info.digest()
+        payload = {}
+        payload['info_hash'] = hash_string
+        payload['peer_id'] = '-TR2820-wa0n562rl3lu'
+        payload['port'] = self.port
+        payload['uploaded'] = 0
+        payload['downloaded'] = 0
+        # pudb.set_trace()
+        payload['left'] = self.tdict['info']['length']
+        payload['numwant'] = 0
+        payload['compact'] = 1
+        payload['supportcrypto'] = 1
+        self.r = requests.get(self.tdict['announce'],
+                              params=payload)
 
 
 def main():
-    # pudb.set_trace()
     tor_dict = tparser.bdecode('torrent.torrent')
     mytorrent = torrent(tor_dict)
     mytorrent.get_request()
-
-    # http://thomasballinger.com:6969/announce?
-    # info_hash=%2b%15%ca%2b%fdH%cd%d7m9%ecU%a3%ab%1b%8aW%18%0a%09
-    # peer_id=-TR2820-jlt0de3likus
-    # port=55251
-    # uploaded=0
-    # downloaded=0
-    # left=1277987
-    # numwant=0
-    # key=556b1696
-    # compact=1&
-    # upportcrypto=1
-    # event=stopped
+    print mytorrent.r.text
+    print mytorrent.r
 
 
 if __name__ == '__main__':
