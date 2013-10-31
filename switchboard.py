8bbfc9e927dfc801d119fb700a86226c478fd8fd
Jeff Blagdon <jeffblagdon@gmail.com>
2013-10-31 15:41:51 -0400

    Creating a directory to store the files I'll write out to

--- switchboard.py
+++ switchboard.py
@@ -1,4 +1,12 @@
+import os
+
+
 class switchboard():
-    def __init__(self, filelist):
-        self.filelist = filelist
+    def __init__(self, dirname, filelist):
+        self.dirname = dirname + '/'
+        self.filelist = filelist[:]
+        os.mkdir(self.dirname)
+        self.outfile = open(os.getcwd() + '/' + self.dirname + 'muff.txt', 'w')
+        self.outfile.write('o hai')
+        self.outfile.close()
         print 'created switchboard'
