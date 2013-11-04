0062c6a2a789510ee3df0d5d6d549883f6c293ed
Jeff Blagdon <jeffblagdon@gmail.com>
2013-11-04 16:25:08 -0500

    Halfway toward limiting download to subset of files

--- switchboard.py
+++ switchboard.py
@@ -1,40 +1,107 @@
 import os
 import pudb
 from collections import namedtuple
-from pprint import pprint
 
 outfile = namedtuple('destination', 'path length')
 
 
 def build_dirs(files):
     for i in files:
-        # pudb.set_trace()
         if len(i['path']) > 1:
             addpath = os.path.join(*i['path'][:-1])
             if addpath and addpath not in os.listdir(os.getcwd()):
                 os.makedirs(addpath)
+                print 'just made path', addpath
+
+
+def get_want_file_pos(filelist):
+    want_file_pos = []
+    print '\nFiles contained:\n'
+    for i in filelist:
+        print(os.path.join(*i['path']))
+    while 1:
+        all_answer = raw_input('\nDo you want all these files? (y/n): ')
+        if all_answer in ('y', 'n'):
+            break
+    if all_answer == 'y':
+        want_file_pos = range(len(filelist))
+        return want_file_pos
+    if all_answer == 'n':
+        for j, tfile in enumerate(filelist):
+            while 1:
+                file_answer = raw_input('Do you want {}? '
+                                        '(y/n): '.format(os.path.join
+                                                        (*tfile['path'])))
+
+                if file_answer in ('y', 'n'):
+                    break
+            if file_answer == 'y':
+                want_file_pos.append(j)
+        print "Here are all the files you want:"
+        for k in want_file_pos:
+            print os.path.join(*filelist[k]['path'])
+        return want_file_pos
+
+
+def get_file_starts(filelist):
+    starts = []
+    total = 0
+    for i in filelist:
+        total += i['length']
+        starts.append(total)
+    return starts
 
 
 class switchboard():
     def __init__(self, dirname, filelist):
         self.dirname = dirname
         self.filelist = filelist[:]
+        self.filestarts = get_file_starts(filelist)
+        self.want_file_pos = get_want_file_pos(self.filelist)
+
+        # want_files is the list of files that i'm interested in. For any
+        # index i, i can:
+        #   - find out which file it belongs to using self.filestarts
+        #   - find out if I'm interested in the file by:
+        #       - getting the file from filelist
+        #       - seeing if i'm interested by checking against wantfiles
+        #
+        # Alternatively, I could:
+        #   - find out which file (really index) it belongs to using
+        #     self.filestarts
+        #   - find out if i'm interested by:
+        #       - seeing if that index is in wantfiles
+
         self.outfiles = []
         self.index = 0
         os.mkdir(self.dirname)
-        os.chdir(os.getcwd() + '/' + self.dirname)
-        build_dirs(self.filelist)
-        for i in self.filelist:
-            thisfile = outfile(path=open(os.path.join(*i['path']), 'w'),
-                               length=i['length'])
+        print 'making directory', self.dirname
+        os.chdir(os.path.join(os.getcwd(), self.dirname))
+        build_dirs(self.filelist[index] for index in self.want_file_pos)
+        for i in self.want_file_pos:
+            thisfile = outfile(path=open(os.path.join(*self.filelist[i]
+                                         ['path']), 'w'),
+                               length=self.filelist[i]['length'])
             self.outfiles.append(thisfile)
-        print '\n\nFiles contained in torrent:'
-        pprint(self.outfiles)
-        print 'Torrent contains {} files'.format(len(self.outfiles))
 
     def seek(self, index):
         self.index = index
 
+# Write is taking a block
+# We've already seeked to the relevant spot
+# If we've got data left over after we write our block and we're NOT
+# interested in the adjacent file, we can just throw it away.
+
+# Right now I have:
+#   self.index -- the current writing position
+#   self.outfiles -- the list of file objects that I'm writing to
+# What I need:
+#   a list to keep track of only the files that I'm interested in.
+#
+# If self.index is not in interested_file_starts:
+#   if len(block) > len(uninterested file)
+#   block = block[len(uninterested file)]
+#   self.index += len(uninterested file)
     def write(self, block):
         abs_end = sum([i.length for i in self.outfiles])
         j = 1
