f8b19227e50a8a497477e9805b9de892815c4075
Jeff Blagdon <jeffblagdon@gmail.com>
2013-11-05 20:44:48 -0500

    Got logic for interested_bitfield working

--- switchboard.py
+++ switchboard.py
@@ -65,7 +65,6 @@ def get_write_file(index=0, file_starts=[0], files=[], outfiles=[]):
             i += 1
     j = 1
     while j <= len(outfiles) + 1:
-        pudb.set_trace()
         if outfiles[-j].fobj.name == os.path.join(*tfile['path']):
             return outfiles[-j]
         else:
@@ -83,28 +82,57 @@ def get_file_start(index=0, file_starts=[]):
             i += 1
 
 
-def get_interested(files=[], want_file_pos=[]):
-    piece_indices = bitarray.bitarray()
-    next_index = 0
-    file_index = 0
+def get_interested(files=[], want_file_pos=[], file_starts=[],
+                   piece_length=0, num_pieces=0):
+    interested_bitfield = bitarray.bitarray()
+    want_index = 0
     j = 0
-    while j < len(files):
-        if file_index in want_file_pos:
-            piece_indices.extend(b'1'*files[file_index]['length'])
-            next_index += files[file_index]['length']
-            file_index += 1
-        else:
-            piece_indices.extend(b'0'*files[file_index]['length'])
-            next_index += files[file_index]['length']
-            file_index += 1
-        j += 1
-    return piece_indices
+    pudb.set_trace()
+    while j < num_pieces:
+        if want_index >= len(want_file_pos):
+            while len(interested_bitfield) < num_pieces:
+                interested_bitfield.append(0)
+            return interested_bitfield
+        piece_start = j * piece_length
+        next_piece_start = piece_start + piece_length
+        file_start = file_starts[want_file_pos[want_index]]
+        next_file_start = (file_starts[want_file_pos[want_index]] +
+                           files[want_file_pos[want_index]]['length'])
+
+        # If the piece starts and ends before the start of the current
+        # interesting file, we don't need it
+        if (piece_start < file_start and
+                next_piece_start < file_start):
+
+                interested_bitfield.append(0)
+                j += 1
+
+        # If the piece starts after the end of the current interesting file,
+        # move to the next interesting file
+        elif piece_start >= next_file_start:
+            want_index += 1
+
+        # If the piece starts after the start of the current interesting file,
+        # we need it
+        elif piece_start >= file_start:
+            interested_bitfield.append(1)
+            j += 1
+
+        # And if the piece ends before the end of the current interesting file,
+        # we need it
+        elif next_piece_start <= next_file_start:
+            interested_bitfield.append(1)
+            j += 1
+    # pudb.set_trace()
+    return interested_bitfield
 
 
 class switchboard():
-    def __init__(self, dirname, file_list):
+    def __init__(self, dirname='', file_list=[], piece_length=0, num_pieces=0):
         self.dirname = dirname
         self.file_list = file_list[:]
+        self.piece_length = piece_length
+        self.num_pieces = num_pieces
         self.file_starts = get_file_starts(file_list)
         self.want_file_pos = get_want_file_pos(self.file_list)
         self.outfiles = []
@@ -120,7 +148,11 @@ class switchboard():
             self.outfiles.append(thisfile)
         self.interested_indices = get_interested(files=self.file_list,
                                                  want_file_pos=
-                                                 self.want_file_pos)
+                                                 self.want_file_pos,
+                                                 num_pieces=self.num_pieces,
+                                                 piece_length=
+                                                 self.piece_length,
+                                                 file_starts=self.file_starts)
         # file_list still OK after init. Who touches switchboard next?
 
     def seek(self, index):
@@ -128,7 +160,6 @@ class switchboard():
 
     def write(self, block):
         while block:
-            pudb.set_trace()
             file_start = get_file_start(index=self.index,
                                         file_starts=self.file_starts)
             write_file = get_write_file(index=self.index,
@@ -157,3 +188,24 @@ class switchboard():
     def close(self):
         for i in self.outfiles:
             i.fobj.close()
+
+
+def main():
+    files = [{'length': 3}, {'length': 3}, {'length': 3}, {'length': 3}, {'length': 3}]
+    want_file_pos = [1, 3]
+    file_starts = [0, 3, 6, 9, 12]
+    piece_length = 4
+    print 'case 1', get_interested(files=files, want_file_pos=want_file_pos,
+                                   file_starts=file_starts,
+                                   piece_length=piece_length, num_pieces=4)
+    files = [{'length': 4}, {'length': 4}, {'length': 4}, {'length': 4}, {'length': 4}]
+    want_file_pos = [1]
+    file_starts = [0, 4, 8, 12, 16]
+    piece_length = 3
+    print 'case 2', get_interested(files=files, want_file_pos=want_file_pos,
+                                   file_starts=file_starts,
+                                   piece_length=piece_length, num_pieces=4)
+
+
+if __name__ == '__main__':
+    main()
