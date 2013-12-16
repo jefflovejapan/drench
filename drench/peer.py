959bf334f0d1d4a310da7893eede5dfe8e7aa9d6
Jeff Blagdon <jeffblagdon@gmail.com>
2013-12-16 01:36:20 -0500

    Still aligning text

--- drench/peer.py
+++ drench/peer.py
@@ -25,6 +25,7 @@ class Peer(object):
         # What am I trying to do?
         # I only want self.location to include keys and vals
         # if val for key in in_location
+        pudb.set_trace()
         self.location = self.set_location(in_location)
 
         self.valid_indices = []
